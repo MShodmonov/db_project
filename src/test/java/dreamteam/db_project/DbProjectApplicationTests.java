bdfb59a4867b5187f687513b9080eda260f74d76
MShodmonov <musobek.shodmonov@mail.ru>
2020-03-04 13:52:45 +0500

    first commit

---
+++ src/test/java/dreamteam/db_project/DbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamteam.db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
